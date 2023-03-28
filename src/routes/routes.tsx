dcc12ecee1f1918fbabf4362c940d4e590b0a327
Gabriel Carriel <ga123carriel2015@gmail.com>
2023-03-28 10:14:18 -0300

    feat: routes file added

---
+++ src/routes/routes.tsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+// import { RegisterPage } from "../pages/RegisterPage";
+// import LoginPage from "../pages/LoginPage";
+// import { DashboardPage } from "../pages/DashboardPage";
+// import { LandingPage } from "../pages/landingPage";
+// import ProtectedRoutes from "./ProtectedRoutes";
+// import { NotFoundPage } from "../pages/NotFoundPage";
+// import { TutorialPage } from "../pages/TutorialPage";
+
+const RoutesMain = () => {
+    return (
+        <>
+            <Routes>
+                <Route path="/" />
+            </Routes>
+        </>
+    );
+};
+
+export default RoutesMain;
